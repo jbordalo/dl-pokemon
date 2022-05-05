8eea860546066c368cfb9e36cad21cf83a10903a
André Palma <andremarquespalma@gmail.com>
2022-05-05 20:48:38 +0100

    Fix Multiclass

--- tp1.py
+++ tp1.py
@@ -69,34 +69,35 @@ def build_multilabel_model():
 def build_multiclass_model():
     inputs = Input(shape=(64, 64, 3), name='inputs')
 
-    layer = Conv2D(32, (3, 3), padding="same")(inputs)
-    layer = Activation("relu")(layer)
+    layer = Conv2D(32, (3, 3), padding="same", activation="relu")(inputs)
     layer = BatchNormalization()(layer)
-    layer = Conv2D(32, (3, 3), padding="same")(layer)
-    layer = Activation("relu")(layer)
+    layer = Conv2D(32, (3, 3), padding="same", activation="relu")(layer)
     layer = BatchNormalization()(layer)
     layer = MaxPooling2D(pool_size=(2, 2))(layer)
 
-    layer = Conv2D(64, (3, 3), padding="same")(layer)
-    layer = Activation("relu")(layer)
+    layer = Conv2D(64, (3, 3), padding="same", activation="relu")(layer)
     layer = BatchNormalization()(layer)
-    layer = Conv2D(64, (3, 3), padding="same")(layer)
-    layer = Activation("relu")(layer)
+    layer = Conv2D(64, (3, 3), padding="same", activation="relu")(layer)
+    layer = BatchNormalization()(layer)
+    layer = Conv2D(64, (3, 3), padding="same", activation="relu")(layer)
     layer = BatchNormalization()(layer)
     layer = MaxPooling2D(pool_size=(2, 2))(layer)
 
     features = Flatten(name='features')(layer)
-    layer = Dense(256)(features)
-    layer = Activation("relu")(layer)
+    layer = Dense(256, activation="relu")(features)
     layer = Dropout(0.5)(layer)
-    layer = Dense(10)(layer)
-    layer = Activation("softmax")(layer)
+    layer = Dense(10, activation="softmax")(layer)
 
     return Model(inputs=inputs, outputs=layer)
 
 
 def multiclass_model(train_x, train_classes, test_x, test_classes):
-    train_X, val_x, train_y, val_y = train_test_split(train_x, train_classes, test_size=500)
+    train_x, val_x, train_y, val_y = train_test_split(train_x, train_classes, test_size=500)
+
+    EPOCHS = 100
+    BATCH_SIZE = 32
+    INIT_LR = 0.01
+    MOMENTUM = 0.9
 
     model = build_multiclass_model()
 
@@ -104,17 +105,25 @@ def multiclass_model(train_x, train_classes, test_x, test_classes):
 
     model.compile(opt, loss='categorical_crossentropy', metrics=['accuracy'])
 
-    history = model.fit(train_X, train_y, validation_data=(val_x, val_y), batch_size=BATCH_SIZE, epochs=EPOCHS)
-    final_training_data = [(l, history.history[l][-1]) for l in history.history]
+    history = model.fit(train_x, train_y, validation_data=(val_x, val_y), batch_size=BATCH_SIZE, epochs=EPOCHS)
 
+    final_training_data = [(l, history.history[l][-1]) for l in history.history]
     print(final_training_data)
-    print(100 - model.evaluate(test_x, test_classes)[1] * 100)
+
+    loss, acc = model.evaluate(test_x, test_classes)
+    print('Multilabel test loss:', loss)
+    print('Multilabel test accuracy:', acc)
 
     plot(history)
 
 
 def multilabel_model(train_x, train_labels, test_x, test_labels):
     train_x, val_x, train_y, val_y = train_test_split(train_x, train_labels, test_size=500)
+
+    EPOCHS = 100
+    BATCH_SIZE = 32
+    INIT_LR = 0.01
+    MOMENTUM = 0.9
 
     model = build_multilabel_model()
 
@@ -123,10 +132,13 @@ def multilabel_model(train_x, train_labels, test_x, test_labels):
     model.compile(opt, loss='binary_crossentropy', metrics=['accuracy'])
 
     history = model.fit(train_x, train_y, validation_data=(val_x, val_y), batch_size=BATCH_SIZE, epochs=EPOCHS)
-    final_training_data = [(l, history.history[l][-1]) for l in history.history]
 
+    final_training_data = [(l, history.history[l][-1]) for l in history.history]
     print(final_training_data)
-    print(100 - model.evaluate(test_x, test_labels)[1] * 100)
+
+    loss, acc = model.evaluate(test_x, test_labels)
+    print('Multilabel test loss:', loss)
+    print('Multilabel test accuracy:', acc)
 
     plot(history)
 
@@ -135,12 +147,7 @@ def segmentation_model(train_x, train_masks, test_x, test_masks):
     return -1
 
 
-if __name__ == '__main__':
-    EPOCHS = 100
-    BATCH_SIZE = 32
-    INIT_LR = 0.01
-    MOMENTUM = 0.9
-
+def main():
     train_x, test_x, train_masks, test_masks, train_classes, train_labels, test_classes, test_labels = load_data().values()
 
     multiclass_model(train_x, train_classes, test_x, test_classes)
@@ -148,3 +155,7 @@ if __name__ == '__main__':
 # multilabel_model(train_x, train_labels, test_x, test_labels)
 
 # segmentation_model(train_x, train_masks, test_x, test_masks)
+
+
+if __name__ == '__main__':
+    main()
